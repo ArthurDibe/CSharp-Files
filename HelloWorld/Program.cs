78a4d2290f8ab759391b17a36999547cfd310bed
Arthur Dibe <dibearthur@gmail.com>
2021-09-09 15:46:43 -0300

    Created a Hello World Application in C#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace appTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello World of " + name + "!");
+            Console.WriteLine($"{name} is cool !");
+        }
+    }
+}
