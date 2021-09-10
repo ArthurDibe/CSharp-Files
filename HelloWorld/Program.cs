54a10787fb1dddbd881e8af307e451dbf6b2dc8b
Arthur Dibe <dibearthur@gmail.com>
2021-09-10 14:43:56 -0300

    Program.cs: Added other examples of String Methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -15,13 +15,26 @@ namespace appTeste
             string nameTrim = name.Trim();
 
             Console.WriteLine();
-            Console.WriteLine("Hello World of |" + name + "| !");
+            Console.WriteLine("Hello World of |" + name + "|");
             Console.WriteLine($"{nameTrim} is cool when Trimmed!");
             Console.WriteLine($"When your Name is UPPER: {nameTrim.ToUpper()}{br}When Your name is lower: {nameTrim.ToLower()}");
             Console.WriteLine("The Length of your name is: " + nameTrim.Length.ToString()); // We have to convert the length into string in order to concatenate it
             Console.WriteLine(string.Format("Using \"string.Format()\" method.......> |{0}| has {1} characters", nameTrim, nameTrim.Length));
             Console.WriteLine(string.Format("Using \"string.Format()\" method to format: {0:C} Dollars is {1:P} Percentage of the World", 1288, 55));
+            Console.WriteLine($"--------------------------------------------------------------------------------------------------------------{br}");
 
+            string phrase = "This phrase was $ composed in [C# Language] with at no $ cost! :)";
+            Console.WriteLine($"Phrase: |{phrase}|");
+            Console.WriteLine("-------------------------------------------------------------------");
+            Console.WriteLine($"Number of characters of the Phrase is: {phrase.Length}");
+            Console.WriteLine("Left Square Braket \"[\" found at index: " + phrase.IndexOf("["));
+            Console.WriteLine("First \"$\" found at index: " + phrase.IndexOf("$"));
+            Console.WriteLine("Last \"$\" found at index: " + phrase.LastIndexOf("$"));
+            Console.WriteLine($"Substring the Phrase, from index 16 up to 10 characters after that: |{phrase.Substring(16, 10)}|{br}");
+
+            string newPhrase = phrase.Replace("C# Language", ".NET Framework");
+            Console.WriteLine($"The new Phrase is: |{newPhrase}|");
+            Console.WriteLine($"--------------------------------------------------------------------------------------------------------------{br}");
 
         }
     }
